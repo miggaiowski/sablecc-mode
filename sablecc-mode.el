59ca9f1023dd5404ba1aa804fe666cfb7f313664
Miguel Gaiowski <miggaiowski@gmail.com>
2011-03-22 10:54:30 -0300

    Fixed bug with \n inside {}

--- sablecc-mode.el
+++ sablecc-mode.el
@@ -3,7 +3,7 @@
   '("Helpers" "Tokens" "Productions" "Package" "Ignored\sTokens" "Abstract\sSyntax\sTree")
   '(("\\[[^\' ']*\\]:" . 'font-lock-doc-face) 
     ("\{\s*->[^\{\}]*\}" . 'font-lock-type-face) 
-    ("\{[^\{\}]'\n'*\}" . 'font-lock-function-name-face) 
+    ("\{[^\{\}\n]*\}" . 'font-lock-function-name-face) 
     ("\'[^\s]*\'" . 'font-lock-doc-face)
     ("[\|=]" . 'font-lock-builtin-face)
     ("[\*\+\?]" . 'font-lock-warning-face)
